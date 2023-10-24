0bcbe3e2f039827289ff2978c9324caae54fb72e
Deekshitha <deekshithachatla@gmail.com>
2023-10-24 17:48:10 +0530

    commited Successfully

---
+++ project_3/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_3
+{
+    internal class Student
+    {
+
+            public string Name { get; }
+            public string Class { get; }
+
+            public Student(string name, string className)
+            {
+                Name = name;
+                Class = className;
+            }
+        }
+    }
+
